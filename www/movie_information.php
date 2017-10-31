b2c7059c27e3a7f064ca5b2e355ed8f274d80c31
smaranki <33140129+smaranki@users.noreply.github.com>
2017-10-30 22:46:07 -0700

    Add files via upload
    ACTUALLY NOW

--- www/movie_information.php
+++ www/movie_information.php
@@ -47,6 +47,8 @@
 	if (!mysql_select_db("CS143", $conn))
 		die(mysql_error());
 	$mid = (int) $_GET["mid"];
+
+
 	## Movie ##
 	$queryM = "SELECT * FROM Movie WHERE id=" . $mid;
 	$resultM = mysql_query($queryM);
@@ -57,6 +59,8 @@
 	echo "MPAA Rating: $row[rating]<br />\n";
 	echo "Producer: $row[company]<br />\n";
 	mysql_free_result($resultM);
+
+
 	## Director ##
 	$queryD = "SELECT DISTINCT CONCAT(first, ' ', last), dob FROM MovieDirector, Director WHERE did=id AND mid=".$mid.";";
 	$resultD = mysql_query($queryD);
@@ -70,6 +74,7 @@
 	}
 	echo "<br />";
 	mysql_free_result($resultD);
+
 	## MovieGenre ##
 	$queryMG = "SELECT genre FROM MovieGenre WHERE mid=" . $mid;	$resultMG = mysql_query($queryMG);
 	if (!$resultMG)
@@ -81,6 +86,7 @@
 	}
 	echo "<br />";
 	mysql_free_result($resultMG);
+
 	## Movie Rating ##
 	$queryR = "SELECT imdb,rot FROM MovieRating WHERE mid=" . $mid;
 	$resultR = mysql_query($queryR);
@@ -90,6 +96,7 @@
 	echo "IMDB Rating: $rowR[0]<br />\n";
 	echo "Rotten Tomatoes Rating: $rowR[1]<br />\n";
 	mysql_free_result($resultR);
+
 	## SALES ##
 	$queryS = "SELECT ticketsSold, totalIncome FROM Sales WHERE mid=" . $mid;
 	$resultS = mysql_query($queryS);
@@ -99,6 +106,7 @@
 	echo "Tickets Sold: $rowS[0]<br />\n";
 	echo "Total Income: $rowS[1]<br />\n";
 	mysql_free_result($resultS);
+
 	## Actors in the Movie ##
 	$queryMA = "SELECT id, CONCAT(first, ' ', last), role FROM Actor, MovieActor WHERE aid = id AND mid=".$mid;
 	$resultMA = mysql_query($queryMA);
@@ -120,26 +128,29 @@
 	}	
 	echo "</table>\n";
 	mysql_free_result($resultMA);
+
+
 	## Reviews ##
+	echo "<h3> Reviews: </h3>";
 	$queryR = "SELECT AVG(rating), COUNT(rating) FROM Review WHERE mid=" . $mid;
 	$resultR = mysql_query($queryR);
 	if(!$resultR)
 		die(mysql_error());
 	$rowR = mysql_fetch_row($resultR);
-	 if (count($row[0]) == 0)
+	 if (count($rowR[0]) == 0)
 		echo "No Reviews yet :( Be the first to Review! ";
 	else
-		echo "Average Score of $$row[0]/5 based on $row[1] people Give your input! . \n";
+		echo "Average Score of $rowR[0]/5 based on $rowR[1] people. \n";
 	mysql_free_result($resultR);
-	$query = "SELECT name, time, rating, comment FROM Review WHERE mid=" . $mid . " ORDER BY time DESC";
-	if(!$result = mysql_query($query))
+	$queryR = "SELECT name, time, rating, comment FROM Review WHERE mid=" . $mid . " ORDER BY time DESC";
+	if(!$resultR = mysql_query($queryR))
 		die("Error executing query: " . mysql_error());
 	echo "<br />";
-	echo "<a href=\"./review.php?mid=$mid\">Give your input too!</a><br/>\n";
-	while ($row = mysql_fetch_row($resultR))
+	echo "<a href=\"./add_review.php?mid=$mid\">Give your input too!</a><br/>\n";
+	while ($rowR = mysql_fetch_row($resultR))
 	{
 		echo "<br/><br/>\n";
-		echo "$row[0] rated this movie a $row[3] at $row[1]. <br/> comment: <br/>\n $row[3]\n";
+		echo "$rowR[0] rated this movie a $rowR[2] at $rowR[1]. <br/> Comment: <br/>\n $rowR[3]\n";
 	}
 	mysql_free_result($result);
 	mysql_close($conn);
